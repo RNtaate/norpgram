e5ccf6d46d707f5f9e3d87e22cd0caa83d1300d8
RNtaate <royhyde14@gmail.com>
2021-11-01 14:16:59 +0300

    Create Signup component to implement signing up using context

--- src/container/Signup.js
+++ src/container/Signup.js
@@ -1,23 +1,54 @@
-import React from 'react';
-import { Card, Form, Button } from 'react-bootstrap';
+import React, {useRef, useState} from 'react';
+import { Card, Form, Button, Alert } from 'react-bootstrap';
+import {useAuth} from '../context/AuthContext';
 
 const Signup = () => {
+
+  let emailRef = useRef();
+  let passwordRef = useRef();
+  let passwordConfirmRef = useRef();
+
+  let {signup} = useAuth();
+
+  let [error, setError] = useState('');
+  let [loading, setLoading] = useState(false);
+
+  let handleSignup = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+
+    if(passwordRef.current.value !==  passwordConfirmRef.current.value){
+      setLoading(false);
+      return setError("Passwords do not match");
+    }
+
+    try {
+      setError("");
+      await signup(emailRef.current.value, passwordRef.current.value)
+    }catch{
+       setError("Failed to create new user");
+    }finally {
+      setLoading(false);
+    }
+  }
+
   return (
     <>
       <Card>
         <Card.Body>
-          <h2>Sign Up</h2>
-          <Form>
+          <h2 className="text-center mb-4">Sign Up</h2>
+          {error && <Alert variant="danger">{error}</Alert>}
+          <Form onSubmit={handleSignup}>
             <Form.Group id="email">
-              <Form.Control type="email" required placeholder="Email"/>
+              <Form.Control type="email" required placeholder="Email" ref={emailRef}/>
             </Form.Group>
             <Form.Group id="password">
-              <Form.Control type="password" required placeholder="Password" className="my-4"/>
+              <Form.Control type="password" required placeholder="Password" className="my-4" ref={passwordRef}/>
             </Form.Group>
             <Form.Group id="password-confirm">
-              <Form.Control type="password" required placeholder="Password Confirm"/>
+              <Form.Control type="password" required placeholder="Password Confirm" ref={passwordConfirmRef}/>
             </Form.Group>
-            <Button type="submit" className="w-100 mt-4">Sign Up</Button>
+            <Button type="submit" className="w-100 mt-4" disabled={loading}>Sign Up</Button>
           </Form>
         </Card.Body>
       </Card>

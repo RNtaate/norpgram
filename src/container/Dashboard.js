a8fc381e9e1eb625c97023d058e485a753c0301b
RNtaate <royhyde14@gmail.com>
2021-11-01 15:39:34 +0300

    Update, design dashboard component
    Implement logout functionality usign AuthContext

--- src/container/Dashboard.js
+++ src/container/Dashboard.js
@@ -1,10 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Card, Button, Alert } from 'react-bootstrap';
+import { Link, useHistory } from 'react-router-dom';
+import { useAuth } from '../context/AuthContext';
 
 const Dashboard = () => {
+
+  let [error, setError] = useState('');
+  let {currentUser, logout} = useAuth();
+  let history = useHistory();
+
+  let handleLogout = async () => {
+    setError('')
+
+    try {
+      await logout();
+      history.push('/login');
+    }catch {
+      setError('Failed to log out.');
+    }
+  }
+
   return (
-    <div>
-      <h3>Dashboard</h3>
-    </div>
+    <>
+      <Card>
+        <Card.Body>
+          <h2 className="text-center mb-4">Dashboard</h2>
+          {error && <Alert variant="danger">{error}</Alert>}
+          <strong>Email: </strong>{currentUser.email}
+          <Link to="/updateprofile" className="btn btn-primary w-100 mt-3">Update Profile</Link>
+        </Card.Body>
+      </Card>
+      <div className="mt-2 text-center w-100">
+        <Button variant="link" onClick={handleLogout}>Log Out</Button>
+      </div>
+    </>
   )
 }
 

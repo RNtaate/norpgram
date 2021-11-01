a57ec7e986bc3efb6012cd68813ca2c49d512dc7
RNtaate <royhyde14@gmail.com>
2021-11-01 15:09:57 +0300

    Add routing to the App container

--- src/container/App.js
+++ src/container/App.js
@@ -1,20 +1,30 @@
 import './App.css';
 import Signup from './Signup';
+import Dashboard from './Dashboard';
+import Login from './Login';
 import { Container } from 'react-bootstrap';
 import { AuthProvider } from '../context/AuthContext';
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 function App() {
   return (
-    <AuthProvider>
-      <Container
-        className="d-flex justify-content-center align-items-center"
-        style={{minHeight: "100vh"}}
-      >
-        <div className="w-100" style={{maxWidth: "400px"}}>
-          <Signup />
-        </div>
-      </Container>
-    </AuthProvider>
+
+    <Container
+      className="d-flex justify-content-center align-items-center"
+      style={{minHeight: "100vh"}}
+    >
+      <div className="w-100" style={{maxWidth: "400px"}}>
+        <Router>
+          <AuthProvider>
+            <Switch>
+              <Route exact path="/" component={Dashboard} />
+              <Route exact path="/signup" component={Signup} />
+              <Route exact path="/login" component={Login} />
+            </Switch>
+          </AuthProvider>
+        </Router>
+      </div>
+    </Container>
   );
 }
 

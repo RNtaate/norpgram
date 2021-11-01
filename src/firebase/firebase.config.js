ec0b1821aa8619e47b1eb38b11212a84a12758a0
RNtaate <royhyde14@gmail.com>
2021-11-01 14:18:05 +0300

    Import getAuth function

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import "firebase/auth";
+import { getAuth } from '@firebase/auth';
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
@@ -11,6 +12,8 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-export const auth = app.auth();
+
+// export const auth = app.auth();
+export const auth = getAuth(app);
 
 export default app;

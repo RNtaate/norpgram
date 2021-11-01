ad034a07d5d31a99f5ff09182e0533e0950415b7
RNtaate <royhyde14@gmail.com>
2021-11-01 12:09:07 +0300

    Import Signup component
    Add more styling using bootstrap

--- src/container/App.js
+++ src/container/App.js
@@ -1,10 +1,17 @@
 import './App.css';
+import Signup from './Signup';
+import { Container } from 'react-bootstrap';
 
 function App() {
   return (
-    <div className="App">
-      <h1>NorpGram</h1>
-    </div>
+    <Container
+      className="d-flex justify-content-center align-items-center"
+      style={{minHeight: "100vh"}}
+    >
+      <div className="w-100" style={{maxWidth: "400px"}}>
+        <Signup />
+      </div>
+    </Container>
   );
 }
 

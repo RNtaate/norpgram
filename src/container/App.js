88136155fa06ac1a75a9b445106fcd31ffff3469
RNtaate <royhyde14@gmail.com>
2021-11-01 14:17:41 +0300

    Import AuthProvider
    User AuthProvider to supply context to all child components

--- src/container/App.js
+++ src/container/App.js
@@ -1,17 +1,20 @@
 import './App.css';
 import Signup from './Signup';
 import { Container } from 'react-bootstrap';
+import { AuthProvider } from '../context/AuthContext';
 
 function App() {
   return (
-    <Container
-      className="d-flex justify-content-center align-items-center"
-      style={{minHeight: "100vh"}}
-    >
-      <div className="w-100" style={{maxWidth: "400px"}}>
-        <Signup />
-      </div>
-    </Container>
+    <AuthProvider>
+      <Container
+        className="d-flex justify-content-center align-items-center"
+        style={{minHeight: "100vh"}}
+      >
+        <div className="w-100" style={{maxWidth: "400px"}}>
+          <Signup />
+        </div>
+      </Container>
+    </AuthProvider>
   );
 }
 

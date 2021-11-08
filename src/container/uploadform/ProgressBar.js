bb96d98b0d12fed955fded02d03078c3eb940836
RNtaate <royhyde14@gmail.com>
2021-11-08 22:13:18 +0300

    Create progressBar component
    Import and use useStorage hook in it to display upload progress

---
+++ src/container/uploadform/ProgressBar.js
@@ -0,0 +1,27 @@
+import React, { useEffect } from 'react'
+import useStorage from '../hooks/useStorage';
+import { useAuth } from '../../context/AuthContext';
+
+const ProgressBar = ({file, setFile}) => {
+
+    let { currentUser } = useAuth();
+    let {progress, imageUrl} = useStorage(file, currentUser);
+
+    console.log({progress, imageUrl});
+
+    useEffect(() => {
+      if(imageUrl) {
+        setFile(null);
+      }
+    }, [imageUrl])
+
+  return (
+    <div
+      className="progress-bar-div"
+      style={{width: progress + "%"}}
+    >
+    </div>
+  )
+}
+
+export default ProgressBar;

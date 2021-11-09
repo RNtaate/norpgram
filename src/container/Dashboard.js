8c411d55532576714322f004113a9881bab890ab
RNtaate <royhyde14@gmail.com>
2021-11-09 13:46:30 +0300

    Remove unwanted code

--- src/container/Dashboard.js
+++ src/container/Dashboard.js
@@ -1,26 +1,8 @@
 import React, { useState } from 'react'
-import { Card, Button, Alert } from 'react-bootstrap';
-import { Link, useHistory } from 'react-router-dom';
-import { useAuth } from '../context/AuthContext';
 import UploadForm from './uploadform/UploadForm';
 import ImageGrid from './ImageGrid';
 
 const Dashboard = () => {
-
-  let [error, setError] = useState('');
-  let {currentUser, logout} = useAuth();
-  let history = useHistory();
-
-  let handleLogout = async () => {
-    setError('')
-
-    try {
-      await logout();
-      history.push('/login');
-    }catch {
-      setError('Failed to log out.');
-    }
-  }
 
   return (
     <div 
